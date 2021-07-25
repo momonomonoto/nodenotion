04d2b5c8a5d74bba1dae0fd9d4a92abd6ee96284
Shaydulin Kirill Ramisovich <shaydulin.k.r@sblogistica.ru>
2021-07-25 09:47:52 +0300

    Ititial files

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const restify = require('restify');
 const server = restify.createServer();
+const cool = require('cool-ascii-faces');
 
 
 const getRealizeTickets = require('./api/get-realized-tickets');
 
 server.get('/ticket', getRealizeTickets);
+server.get('/cool', (req, res, next) => res.send(cool()));
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
