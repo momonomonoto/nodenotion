6f922012136bf66af31f94a63ba9a3e6e8459583
Shaydulin Kirill Ramisovich <shaydulin.k.r@sblogistica.ru>
2021-07-25 11:09:10 +0300

    Refactoring package.json

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+
+const getRealizeTickets = require('./api/get-realized-tickets');
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req, res) => res.send(cool()))
+    .get('/cool', getRealizeTickets)
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
