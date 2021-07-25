4a0d43bfac8cf8e9bfc5abe21bd5010c3c686271
Shaydulin Kirill Ramisovich <shaydulin.k.r@sblogistica.ru>
2021-07-25 11:17:17 +0300

    Get realized tickets

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-const getRealizeTickets = require('./api/get-realized-tickets');
+const getRealizedTickets = require('./api/get-realized-tickets');
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', getRealizeTickets)
+    .get('/tickets/realized', getRealizedTickets)
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
