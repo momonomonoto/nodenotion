b5d17fbe9ce709b50c12a47285914df4cfb8ed8b
Shaydulin Kirill Ramisovich <shaydulin.k.r@sblogistica.ru>
2021-07-29 16:34:54 +0300

    Update ticker separator

--- utils/get-reatable-title-from-realized-data/index.js
+++ utils/get-reatable-title-from-realized-data/index.js
@@ -1,4 +1,4 @@
-const createLabelTicket = ticket=> ticket.name+ '. ' +'Тикеров: '+ticket.amount+ '. '+ ticket.percent+'%';
+const createLabelTicket = ticket=> ticket.name+ '. ' +'Тикеров: '+ticket.amount+ ' - '+ ticket.percent+'%';
 
 const getReatableTitleFromRealizedData = list => list.reduce((ticketData, ticket)=> ({...ticketData, [createLabelTicket(ticket)]:ticket.amount}),{});
 
