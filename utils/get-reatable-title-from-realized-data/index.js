43d35e1a874da1f06c6f168ae059ad672b6128fd
Shaydulin Kirill Ramisovich <shaydulin.k.r@sblogistica.ru>
2021-08-14 11:19:00 +0300

    Add no point text

--- utils/get-reatable-title-from-realized-data/index.js
+++ utils/get-reatable-title-from-realized-data/index.js
@@ -1,4 +1,6 @@
-const createLabelTicket = ticket=> ticket.name+ '. ' +'Тикеров: '+ticket.amount+ ' - '+ ticket.percent+'%';
+const addNoPointText = name => name === 'Реализовано' || name === 'Не реализовано'? ' (ТВХ нет)':'';
+
+const createLabelTicket = ticket=> ticket.name+ '. ' +'Тикеров: '+ticket.amount+ ' - '+ ticket.percent+'%'+addNoPointText(ticket.name);
 
 const getReatableTitleFromRealizedData = list => list.reduce((ticketData, ticket)=> ({...ticketData, [createLabelTicket(ticket)]:ticket.amount}),{});
 
